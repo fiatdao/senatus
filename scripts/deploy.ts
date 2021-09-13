b6d554d59224e1738da5e8cdf1c3051b83750e53
George Spasov <spasov05203@gmail.com>
2021-09-13 13:57:57 +0300

    Deployment scripts udpdates

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,16 +1,18 @@
 import hardhat from 'hardhat';
 const ethers = hardhat.ethers;
 
-async function deploy (kernel: string) {
+async function deploy(kernel: string) {
     await hardhat.run('compile');
 
     const Governance = await ethers.getContractFactory('Governance');
     const governanceInstance = await Governance.deploy();
     console.log('Deploying Governance, please wait...');
     await governanceInstance.deployed();
+    console.log(`Governance Deployed at ${governanceInstance.address}`)
 
     console.log('Initializing Governance + waiting block confirmations, please wait...');
     const initTx = await governanceInstance.initialize(kernel);
+    console.log('Initialized')
     await initTx.wait(5);
 
     console.log('Verifying contract on Etherscan...');
