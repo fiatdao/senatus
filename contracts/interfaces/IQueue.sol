0ff9fe33a46bbcf614fa52e8118c70626ec95a2f
Kristian Veselinov <thcrnk@gmail.com>
2021-10-27 14:10:00 +0300

    Bridge -> Queue

--- contracts/interfaces/IQueue.sol
+++ contracts/interfaces/IQueue.sol
@@ -2,7 +2,7 @@
 pragma solidity 0.7.6;
 pragma experimental ABIEncoderV2;
 
-interface IBridge {
+interface IQueue {
     function queuedTransactions(bytes32 txHash) external view returns (bool);
 
     function queueTransaction(address target, uint256 value, string memory signature, bytes memory data, uint256 eta) external returns (bytes32);
