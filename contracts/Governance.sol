b831136af2d7cbaeabec11baaa04a6222418be23
Daniel Luca (@CleanUnicorn) <cleanunicorn@protonmail.com>
2021-10-28 18:38:15 +0000

    🐎 Cache storage length for gas discount

--- contracts/Governance.sol
+++ contracts/Governance.sol
@@ -195,7 +195,8 @@ contract Governance is Queue {
         uint256 eta = proposal.createTime + proposal.parameters.warmUpDuration + proposal.parameters.activeDuration + proposal.parameters.queueDuration;
         proposal.eta = eta;
 
-        for (uint256 i = 0; i < proposal.targets.length; i++) {
+        uint256 proposalTargetsLength = proposal.targets.length;
+        for (uint256 i = 0; i < proposalTargetsLength; i++) {
             require(
                 !queuedTransactions[_getTxHash(proposal.targets[i], proposal.values[i], proposal.signatures[i], proposal.calldatas[i], eta)],
                 "proposal action already queued at eta"
@@ -213,7 +214,8 @@ contract Governance is Queue {
         Proposal storage proposal = proposals[proposalId];
         proposal.executed = true;
 
-        for (uint256 i = 0; i < proposal.targets.length; i++) {
+        uint256 proposalTargetsLength = proposal.targets.length;
+        for (uint256 i = 0; i < proposalTargetsLength; i++) {
             executeTransaction(proposal.targets[i], proposal.values[i], proposal.signatures[i], proposal.calldatas[i], proposal.eta);
         }
 
@@ -227,7 +229,8 @@ contract Governance is Queue {
         Proposal storage proposal = proposals[proposalId];
         proposal.canceled = true;
 
-        for (uint256 i = 0; i < proposal.targets.length; i++) {
+        uint256 proposalTargetsLength = proposal.targets.length;
+        for (uint256 i = 0; i < proposalTargetsLength; i++) {
             cancelTransaction(proposal.targets[i], proposal.values[i], proposal.signatures[i], proposal.calldatas[i], proposal.eta);
         }
 
